35095c3e79dfc180725d0753b5b9f301ea0f616e
don-E Merson <don.e.merson1966@gmail.com>
2013-07-03 15:20:20 -0700

    test

---
+++ RegWhyWebApp.Tests/GetZeroOrOneTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegWhy;
+
+namespace RegWhyWebApp.Tests
+{
+    [TestClass]
+    public class GetZeroOrOneTests
+    {
+        [TestMethod]
+        public void GetZeroOrOneTest()
+        {
+            var zero = new RegWhy.GetZeroOrOne(
+               new List<IRegWhat>
+                    {
+                        new GetLiteral("t")
+                    });
+
+            Assert.AreEqual("t*", zero.RegExValue);
+            var more = new RegWhy.GetZeroOrOne(
+                new List<IRegWhat>
+                    {
+                        new GetLiteral("http")
+                    });
+            Assert.AreEqual("(http)*", more.RegExValue);
+        }
+    }
+}
