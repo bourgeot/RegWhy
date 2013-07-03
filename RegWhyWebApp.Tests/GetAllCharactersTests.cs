01ad43a911a2f96fd909584fcce091f0264cef0a
don-E Merson <don.e.merson1966@gmail.com>
2013-07-03 15:20:02 -0700

    tests.

---
+++ RegWhyWebApp.Tests/GetAllCharactersTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegWhy;
+
+namespace RegWhyWebApp.Tests
+{
+    [TestClass]
+    public class GetAllCharactersTests
+    {
+        [TestMethod]
+        public void GetAllCharacterTest()
+        {
+            var all = new RegWhy.RegWhy(new Collection<IRegWhat>
+                                            {
+                                                new GetLiteral(RegWhy.SpecialCharacters.AllCharactersButNewLine)
+                                            });
+            Assert.AreEqual(".",all.RegExValue);
+            Assert.AreEqual("Get exactly .", all.Explanation);
+        }
+    }
+}
