6bb95664551ef18b2b07c3b1434b624f2df7320c
don-E Merson <don.e.merson1966@gmail.com>
2013-07-03 15:14:54 -0700

    sdf

---
+++ RegWhy/GetOneOrMore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace RegWhy
+{
+    public class GetOneOrMore : IRegWhat
+    {
+        public GetOneOrMore(IList<IRegWhat> optionalItem)
+        {
+            RegExstring = "";
+            foreach (IRegWhat regWhat in optionalItem)
+            {
+                RegExstring += regWhat.RegExValue;
+            }
+        }
+
+        private string RegExstring { get; set; }
+
+        #region IRegWhat Members
+
+        public string Explanation
+        {
+            get { return "One or more of" + RegExstring; }
+        }
+
+        public string RegExValue
+        {
+            get
+            {
+                if (RegExstring.Length > 1)
+                {
+                    RegExstring = "(" + RegExstring + ")";
+                }
+                return RegExstring + "+";
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ RegWhy/ZeroOrOne.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegWhy
+{
+    public class GetZeroOrOne : IRegWhat
+    {
+
+        public string RegExstring { get; set; }
+        public GetZeroOrOne(IList<IRegWhat> optionalItem)
+        {
+            RegExstring = "";
+            foreach (var regWhat in optionalItem)
+            {
+                RegExstring += regWhat.RegExValue;
+            }
+
+        }
+        public string Explanation
+        {
+            get { return "An optional character (zero or one of"; }
+        }
+
+        public string RegExValue
+        {
+            get
+            {
+                if (RegExstring.Length > 1)
+                {
+                    RegExstring = "(" + RegExstring + ")";
+                }
+                return RegExstring + "*";
+            }
+        }
+    }
+}
