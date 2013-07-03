acaf83bd240d8b985108545a8613f8aa31610bdd
don-E Merson <don.e.merson1966@gmail.com>
2013-07-03 15:20:27 -0700

    tests

---
+++ RegWhyWebApp.Tests/LiteralTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegWhyWebApp.Tests
+{
+    [TestClass]
+    public class LiteralTests
+    {
+        [TestMethod]
+        public void LiteralTest()
+        {
+            var lit =new  RegWhy.GetLiteral("test");
+            Assert.AreEqual("test",lit.RegExValue);
+            Assert.AreEqual("Get exactly test",lit.Explanation);
+            
+            
+        }
+        [ExpectedException(typeof(FormatException))]
+        public void ExpectLiteralError()
+        {
+            var lit = new RegWhy.GetLiteral("");
+        }
+    }
+}
---
+++ RegWhyWebApp.Tests/RegWhyClassTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegWhy;
+
+namespace RegWhyWebApp.Tests
+{
+    [TestClass]
+    public class RegWhyClassTests
+    {
+        [TestMethod]
+        public void BlankTest()
+        {
+            var rg = new RegWhy.RegWhy(new Collection<IRegWhat>() {new GetLiteral("x")});
+            Assert.AreEqual("x", rg.RegExValue);
+            Assert.AreEqual("", rg.GetTextToSearch());
+            var regex = new Regex("x");
+            Assert.AreEqual(regex.IsMatch("x"), rg.ToRegEx().IsMatch("x"));
+        }
+
+        [TestMethod]
+        public void TestList()
+        {
+            var rg = new RegWhy.RegWhy(new Collection<IRegWhat>() { new GetLiteral("x") });
+            Assert.AreEqual("x", rg.RegExValue);
+            Assert.AreEqual("", rg.GetTextToSearch());
+        }
+}
+}
